9b59df38c04ea4e6c512117de5553f48160a713a
RomanMykush <roman.mykush.dev@gmail.com>
2023-08-29 14:40:53 +0300

    Added logger and tray icon
    Additionaly removed MainWindow

--- App.axaml.cs
+++ App.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -6,18 +7,40 @@ namespace SimpleTtsPrompt;
 
 public partial class App : Application
 {
+    private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
     public override void Initialize()
     {
+        // Setting up logger
+        var config = new NLog.Config.LoggingConfiguration();
+    #if DEBUG
+        var target = new NLog.Targets.ConsoleTarget("log-console");
+    #else
+        var target = new NLog.Targets.FileTarget("log-file") { FileName = "logs.log" };
+    #endif
+        config.AddRule(NLog.LogLevel.Trace, NLog.LogLevel.Fatal, target);
+        NLog.LogManager.Configuration = config;
+        // Avalonia UI stuff
         AvaloniaXamlLoader.Load(this);
+                
+        DataContext = this;
     }
 
     public override void OnFrameworkInitializationCompleted()
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            desktop.MainWindow = new MainWindow();
-        }
-
         base.OnFrameworkInitializationCompleted();
+    }
+
+    public void OpenPrompt()
+    {
+        // TODO: Implement dialog window for text prompt
+
+    }
+
+    public void Exit()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            desktop.Shutdown();
+
+        throw new NotImplementedException("Exit method wasn't implement for this platform");
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia;
+using Avalonia.Controls;
 using System;
 
 namespace SimpleTtsPrompt;
@@ -10,7 +11,7 @@ class Program
     // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+        .StartWithClassicDesktopLifetime(args, ShutdownMode.OnExplicitShutdown);
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
